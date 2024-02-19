469506ea5ffe9de21117cba636d10107037820f9
Levan_Kurkhuli <levan_kurkhuli@epam.com>
2024-02-19 23:42:25 +0200

    grid created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from  '@angular/common/http';
 import { GridComponent } from './components/grid/grid.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GridComponent } from './components/grid/grid.component';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    GridComponent
+    GridComponent,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
